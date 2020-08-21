8a52e7723e53f2ff8b63cdb851538f7a9ab4152d
Craig Spannring <craig.spannring@montanainstruments.com>
2020-08-21 11:03:39 -0600

    Try to handle indentation in lambdas better.

--- dot.elisp-redo2018/setup_stuff_for_cc_mode.el
+++ dot.elisp-redo2018/setup_stuff_for_cc_mode.el
@@ -95,7 +95,7 @@ more pleasant.")
                              (member-init-intro . 0)
                              (substatement-open . 0)
                              (case-label . +)
-                             (inlambda . 0)
+                             (inlambda . -)
                              (arglist-intro . +))))))
 
 (message "about to set c-style-alist")
