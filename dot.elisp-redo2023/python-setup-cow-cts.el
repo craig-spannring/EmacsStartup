ad3f0fe7cf086e915ebfd3e213f81918a67595e2
Craig Spannring <craig.spannring@montanainstruments.com>
2024-03-15 15:45:25 -0600

    Indesisive.  Do we want eglot or lspmode?

--- dot.elisp-redo2023/python-setup-cow-cts.el
+++ dot.elisp-redo2023/python-setup-cow-cts.el
@@ -2,23 +2,38 @@
 
 (require 'package-helpers-cts)
 
-(message "Installing?")
-(install-and-require-packages '(lsp-mode lsp-ui))
-
 (require 'lsp-common-cts)
 
-(use-package lsp-mode
-  :ensure t
-  :commands (lsp lsp-deferred)
-  :config
-  (setq lsp-auto-configure t))
 
-(use-package lsp-mode
-  :hook (python-mode . lsp-deferred) ; Replace 'java-mode' with the appropriate major mode
-  :commands (lsp lsp-deferred)
-  :config
-  ; (lsp-enable-which-key-integration t)
-  (setq lsp-auto-guess-root t) ; Automatically guess project root
-  ) ; Enable which-key integration for better discovery
+(cond
+ ((equal cts-which-lsp-package 'use-lsp)
+  (use-package lsp-mode
+               :ensure t
+               :commands (lsp lsp-deferred)
+               :config
+               (setq lsp-auto-configure t))
+  
+  (use-package lsp-mode
+               :hook (python-mode . lsp-deferred) ; Replace 'java-mode' with the appropriate major mode
+               :commands (lsp lsp-deferred)
+               :config
+                                        ; (lsp-enable-which-key-integration t)
+               (setq lsp-auto-guess-root t) ; Automatically guess project root
+               ) ; Enable which-key integration for better discovery
+  (install-and-require-packages '(lsp-mode lsp-ui))
+  )
+ ((equal cts-which-lsp-package 'use-eglot)
+  (use-package eglot
+               :ensure t
+               :config
+               (add-to-list 'eglot-server-programs '(python-mode . ("pylsp")))
+
+               (setq-default eglot-workspace-configuration
+                             '((:pylsp . (:configurationSources ["flake8"] :plugins (:pycodestyle (:enabled nil) :mccabe (:enabled nil) :flake8 (:enabled t))))))
+
+               :hook
+               ((python-mode . eglot-ensure)))
+  ))
+  
 
 (provide 'python-setup-cow-cts)
