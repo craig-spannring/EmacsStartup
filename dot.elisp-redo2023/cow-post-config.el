e5bfa7a2783f80fb23e284392df08376fe51e8cb
Craig Spannring <craig.spannring@montanainstruments.com>
2024-03-20 16:55:57 -0600

    Show clock on the status bar.

--- dot.elisp-redo2023/cow-post-config.el
+++ dot.elisp-redo2023/cow-post-config.el
@@ -1,1 +1,9 @@
-;;; Settings that should be made after loading our packages. 
+;;; Settings that should be made after loading our packages.
+;;;
+;;; This is a good spot for user choices, e.g. should clock be displayed?
+
+(defcustom cow-show-clock t "Show clock in mode"
+  :type 'boolean
+  :group 'cow-emacs-conf)
+(display-time-mode (if cow-show-clock 1 -1))
+
