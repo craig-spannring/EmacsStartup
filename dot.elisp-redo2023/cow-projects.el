ef0464fbbb26fe9bb68c47f2848c2a9ac75a744d
Craig Spannring <craig.spannring@montanainstruments.com>
2024-03-20 16:58:15 -0600

    Better docs.

--- dot.elisp-redo2023/cow-projects.el
+++ dot.elisp-redo2023/cow-projects.el
@@ -2,36 +2,10 @@
 (use-package projectile
              :ensure t)
 
-(defvar _cow-proj-handler-map nil
-"Lookup table of project handlers
-
-This lookup table is a list of dotted pairs.  
-
-The first item in each dotted is a predicate function.  This
-predicate function takes 3 parmeters:
-   -name        Current (partial) filename to expand.
-   -predicate   A predicate function to filter out files
-                that shouldn't be considered. 
-   -how         See 'Programmed Completion' in elisp manual
-
-The second item in each dotted pair is a function that will setup 
-for the given project.")
-
-(defun _cow-register-project-type (proj)
-  (if (not (member proj _cow-proj-handler-map))
-      (progn
-	(message "trying to add %s" proj)
-	(setq _cow-proj-handler-map (cons proj _cow-proj-handler-map)))
-    (message "not adding %s" proj))
-  )
-
-(defun mypred (name predicate how) t)
-(defun mysetup () nil)
  
-(_cow-register-project-type '(mypred mysetup))
-(_cow-register-project-type '(mypred mysetup))
-
 (defun cow-switch-project (proj-file)
+  "Herd the cow into a different project.
+"
   (interactive (list (expand-file-name
 		      (completing-read
 		       ;; PROMPT
@@ -57,5 +31,48 @@ for the given project.")
   (message "proj-file is %s" proj-file))
 
 ;; (defalias 'switch-project cow-switch-project)
+
+(defun cow-register-project-type (proj)
+  "Register project type with the cow. 
+
+PROJ is a dotted pair. The first item in the dotted is a
+predicate function.  This predicate function takes 3 parmeters:
+   -name        Current (partial) filename to expand.
+   -predicate   A predicate function to filter out files
+                that shouldn't be considered. 
+   -how         See 'Programmed Completion' in elisp manual
+                  nil   
+                  t
+                  lambda
+                  (boundaries . suffix)
+                  metadata
+                  Any other value
+
+The second item in each dotted pair is a function that will setup
+for the given project."
+  
+  (if (not (member proj _cow-proj-handler-map))
+      (progn
+	(message "trying to add %s" proj)
+	(setq _cow-proj-handler-map (cons proj _cow-proj-handler-map)))
+    (message "not adding %s" proj))
+  )
+
+(defvar _cow-proj-handler-map nil
+"Lookup table of project handlers.
+
+This lookup table is a list of dotted pairs.  See
+cow-register-project-type for information about the format of the
+dotted pair.")
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; remove this block                              ;;
+(defun myexpander  (name predicate how) t)        ;;
+(defun mysetup     (name)               nil)	  ;;
+(cow-register-project-type '(myexpander mysetup)) ;;
+(cow-register-project-type '(myexpander mysetup)) ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (provide 'cow-projects)
