9ed8c11991215a0c2db36c43e59e5764cfa804fd
Craig Spannring <craig.spannring@montanainstruments.com>
2024-03-20 16:55:29 -0600

    snapshot

--- dot.elisp-redo2023/cow-cpp-setup-rtags.el
+++ dot.elisp-redo2023/cow-cpp-setup-rtags.el
@@ -1,6 +1,8 @@
 ;;; Use rtags for C++ support
 
 (require 'cow-package-helpers)
+(require 'cow-projects)
+
 
 (use-package company        :ensure t)
 (use-package company-rtags  :ensure t)
@@ -15,6 +17,12 @@
              :config (setq rtags-display-result-backend      'helm))
 
              
-             
+(defun _cow-expand-rtag-proj (name predicate how) 
+  nil)
+
+(defun _cow-setup-rtag-proj (name)
+  t)
+
+(cow-register-project-type '(_cow-expand-rtag-proj _cow-setup-rtag-proj))
 
 (provide 'cow-cpp-setup-rtags)
