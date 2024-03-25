274c218c3b5af7ffc478e60d22ffe92cfacc3284
Craig Spannring <craig.spannring@montanainstruments.com>
2024-03-25 12:05:44 -0600

    Setup parameters for grep-find

--- dot.elisp-redo2023/cow-dired.el
+++ dot.elisp-redo2023/cow-dired.el
@@ -1,10 +1,43 @@
 ;; Bring in dired and setup some global keybinds 
 (require 'dired-x)
-(global-set-key "\C-cf" 'find-dired)
-(global-set-key "\C-cn" 'find-name-dired)
-(global-set-key "\C-cl" 'find-grep-dired)
-
 
 (use-package ztree :ensure t)
 
 (provide 'cow-dired)
+
+
+;;; Setup for grep-find
+(let* ((windows-nt (string= system-type "windows-nt"))
+       (predicates (concat  ;; List of files and directories to exclude from the find
+                    (if t          " \\( ! -regex '.*/Debug[a-zA-Z0-9]*Board/.*' \\) " "")
+
+                    (if windows-nt " \\( ! -regex '.*/Output/Debug/.*'   \\) "         "")     
+                    (if windows-nt " \\( ! -regex '.*/Output/Release/.*' \\) "         "")
+                                   
+                    (if t          " \\( ! -regex '.*/.svn/.*' \\) "                   "")
+                    (if t          " \\( ! -name '*~' \\) "                            "")
+                    (if t          " \\( ! -name 'BROWSE' \\) "                        "")
+                                   
+                    (if t          " \\( ! -name '.QTmakefile*' \\) "                  "")
+                    (if t          " \\( ! -name 'moc_*\\.cpp' \\) "                   "")
+                    (if t          " \\( ! -regex '.*/\\.obj.*' \\) "                  "")
+                    (if t          " \\( ! -regex '.*/\\.obj.*' \\) "                  "")))
+       (pattern (cond
+                 (windows-nt
+                  (format "gfind . %s -type f -print0 | xargs -0 -e grep -nIH -e  " predicates))
+                 (t
+		  (format "find . %s -type f -exec grep --color -nIH -e   {} +"     predicates))))
+       (index (cond
+                 (windows-nt   (length pattern))
+                 (t            (- (length pattern) 5)))))
+  (cond 
+   (windows-nt
+    (setq find-program            "gfind")
+    (setq find-dired-find-program "gfind")
+    (setq grep-find-command       (cons pattern index)))
+   (t
+    (cond 
+     ((fboundp 'grep-apply-setting)
+      (grep-apply-setting 'grep-find-command (cons pattern index)))
+     (t
+      (setq grep-find-command (cons pattern index)))))))
