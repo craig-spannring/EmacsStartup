065091b79580aae0d2d96b3424631a4ab7144e31
Craig Spannring <craig.spannring@montanainstruments.com>
2024-03-25 12:02:41 -0600

    Clean up.

--- dot.elisp-redo2018/setup_dired.el
+++ dot.elisp-redo2018/setup_dired.el
@@ -4,39 +4,37 @@
 (require 'dired-x)
 
 
-(let* ((predicates (concat
-                    (if t                                         " \\( ! -regex '.*/Debug[a-zA-Z0-9]*Board/.*' \\) " "")
+(let* ((windows-nt (string= system-type "windows-nt"))
+       (predicates (concat  ;; List of files and directories to exclude from the find
+                    (if t          " \\( ! -regex '.*/Debug[a-zA-Z0-9]*Board/.*' \\) " "")
 
-                    (if (string= system-type "windows-nt")        " \\( ! -regex '.*/Output/Debug/.*'   \\) " "")
-                    (if (string= system-type "windows-nt")        " \\( ! -regex '.*/Output/Release/.*' \\) " "")
-                    
-                    (if t                                         " \\( ! -regex '.*/.svn/.*' \\) "           "")
-                    (if t                                         " \\( ! -name '*~' \\) "                    "")
-                    (if t                                         " \\( ! -name 'BROWSE' \\) "                "")
-                    
-                    (if (not (string= system-type "windows-nt"))  " \\( ! -name '.QTmakefile*' \\) "          "")
-                    (if (not (string= system-type "windows-nt"))  " \\( ! -name 'moc_*\\.cpp' \\) "           "")
-                    (if (not (string= system-type "windows-nt"))  " \\( ! -regex '.*/\\.obj.*' \\) "          "")
-                    (if (not (string= system-type "windows-nt"))  " \\( ! -regex '.*/\\.obj.*' \\) "          "")))
+                    (if windows-nt " \\( ! -regex '.*/Output/Debug/.*'   \\) "         "")     
+                    (if windows-nt " \\( ! -regex '.*/Output/Release/.*' \\) "         "")
+                                   
+                    (if t          " \\( ! -regex '.*/.svn/.*' \\) "                   "")
+                    (if t          " \\( ! -name '*~' \\) "                            "")
+                    (if t          " \\( ! -name 'BROWSE' \\) "                        "")
+                                   
+                    (if t          " \\( ! -name '.QTmakefile*' \\) "                  "")
+                    (if t          " \\( ! -name 'moc_*\\.cpp' \\) "                   "")
+                    (if t          " \\( ! -regex '.*/\\.obj.*' \\) "                  "")
+                    (if t          " \\( ! -regex '.*/\\.obj.*' \\) "                  "")))
        (pattern (cond
-                 ((string= system-type "windows-nt")
+                 (windows-nt
                   (format "gfind . %s -type f -print0 | xargs -0 -e grep -nIH -e  " predicates))
-                 ;((string= system-type "darwin")
-                 ; (format "find . %s -type f -print0 | xargs -0 grep -nIH -e " predicates))
                  (t
-		  (format "find . %s -type f -exec grep --color -nIH -e   {} +" predicates))))
+		  (format "find . %s -type f -exec grep --color -nIH -e   {} +"     predicates))))
        (index (cond
-                 ((string= system-type "windows-nt")   (length pattern))
-                 ;((string= system-type "darwin")       (length pattern))
-                 (t                                    (- (length pattern) 5)))))
-  
+                 (windows-nt   (length pattern))
+                 (t            (- (length pattern) 5)))))
   (cond 
-   ((string= system-type "windows-nt")
-    (setq find-program "gfind")
+   (windows-nt
+    (setq find-program            "gfind")
     (setq find-dired-find-program "gfind")
-    (setq grep-find-command (cons pattern index)))
+    (setq grep-find-command       (cons pattern index)))
    (t
     (cond 
      ((fboundp 'grep-apply-setting)
       (grep-apply-setting 'grep-find-command (cons pattern index)))
-     (t (setq grep-find-command (cons pattern index)))))))
+     (t
+      (setq grep-find-command (cons pattern index)))))))
