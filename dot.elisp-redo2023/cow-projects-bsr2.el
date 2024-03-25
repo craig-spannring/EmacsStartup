b53a42132ef6af87e665716c48603dc980a3bdee
Craig Spannring <craig.spannring@montanainstruments.com>
2024-03-25 11:09:10 -0600

    File completion for bsr2 projects.

--- dot.elisp-redo2023/cow-projects-bsr2.el
+++ dot.elisp-redo2023/cow-projects-bsr2.el
@@ -16,37 +16,10 @@
              :config (setq rtags-use_helm            t)
              :config (setq rtags-display-result-backend      'helm))
 
-             
-(defun _cow-expand-bsr2-proj (name predicate how)
-  
-  (message "in (_cow-expand-bsr2-proj %s %s %s)" name predicate how)
-  (cond
-   ((null how)
-    ;; This is a try-completion operation
-    (cond
-     ((equal name "")
-      (message "returning %s" default-directory)
-      default-directory)
-     (t
-      (message "returning nil")
-      nil)))
-   ((equal how t)
-    ;; This an all-completions operation 
-    nil)
-   ((equal how 'lambda)
-    ;; This a test-completions operation
-    (funcall predicate name))
-   ((and (consp how) (equal (car how) 'boundaries))
-    nil)
-   ((eq how 'metadata)
-    '(metadata '((category file))))
-   (t
-    (message "how is unhandled.  |%s|" how)
-    nil)))
-
 (defun _cow-predicate-bsr2-proj (name)
   "Determine if NAME indicates a bsr2 project"
-  (and (file-exists-p name) (equal (file-name-nondirectory name) "bsr2")))
+	  (and (file-exists-p name)
+	       (equal (file-name-nondirectory name) "bsr2")))
 
 (defun _cow-setup-bsr2-proj (proj-file)
   (message "need to setup for project %s" proj-file)
@@ -65,8 +38,7 @@
 					    '(compile-history . 1))))
 				  (compile cmd))))))
 
-(cow-register-project-type '_cow-expand-bsr2-proj
-                           '_cow-predicate-bsr2-proj
+(cow-register-project-type '_cow-predicate-bsr2-proj
                            '_cow-setup-bsr2-proj)
 
 (provide 'cow-projects-bsr2)
--- dot.elisp-redo2023/cow-projects.el
+++ dot.elisp-redo2023/cow-projects.el
@@ -5,53 +5,27 @@
 
  
 (defun cow-load-project (proj-file)
-  "Herd the cow into a different project.
+  "Move the cow into a different project.
 "
   (interactive (list
 		(expand-file-name
-		 (completing-read
-		  ;; PROMPT
-		  "Project File: "  
-		  ;; COLLECTION
-		  #'(lambda (name predicate how)
-		      ;; For each one of the project types registered by
-		      ;; cow-register-project-type, call the collection
-		      ;; function
-                      (message "In collection function.  how=%s" how)
-                      (let*
-			  ((outcomes (mapcar
-				      #'(lambda (proj-bundle)
-					  (let*
-					      ((completion (car  proj-bundle))
-					       (predicate  (cadr proj-bundle)))
-					    (funcall completion
-						     name
-						     predicate
-						     how)))
-                                      _cow-proj-handler)))
-                        (message "outcomes is %s" outcomes)
-			;; TODO figure out how to colapse all the outcomes into one
-			(cond
-			 ((member t outcomes) t)
-			 (t nil))))
-		  ;; PREDICATE
-		  #'(lambda (x) (message "Note- Shouldn't get here. |%s|" x))
-		  ;; REQUIRE-MATCH
-		  t                 
-		  ;; INITIAL-INPUT
-		  nil              
-		  ;; HIST
-		  nil 
-		  ;; DEF
-		  nil
-		  ;; INHERIT-INPUT-METHOD
-		  nil))))
+		 (read-file-name "Project File: " ;; PROMPT
+				 nil              ;; DIR
+				 nil              ;; DEFAULT-FILENAME
+				 t                ;; MUSTMATCH
+				 nil              ;; INITIAL
+				 #'(lambda (f)    ;; PREDICATE
+				     (or (mapcar
+					  #'(lambda (h)
+					      (funcall (car h) f))
+					  _cow-proj-handler)))))))
+ 
   (let* ((setup (cdr (car (cl-remove-if-not
 		      #'(lambda (x) (car x))
 		      (mapcar #'(lambda (proj-bundle)
 				  (let*
-				      ((setup      (caddr proj-bundle))
-				       (predicate  (cadr  proj-bundle)))
+				      ((setup      (cadr proj-bundle))
+				       (predicate  (car  proj-bundle)))
 				    (cons (funcall predicate proj-file) setup)))
 			      _cow-proj-handler))))))
     (message "proj-file is %s" proj-file)
@@ -68,7 +42,7 @@
     (funcall compile-func)))
     
 
-(defun cow-register-project-type (completion predicate setup)
+(defun cow-register-project-type (predicate setup)
   "Register project type with the cow. 
 
 PROJ is a list of three items. The first item in the dotted is a
@@ -105,23 +79,66 @@ should return a plist with the keys
   compile
   proj-file
 "
-  (let* ((proj (list completion predicate setup)))
+  (let* ((proj (list predicate setup)))
     (if (not (member proj _cow-proj-handler))
 	(progn
 	  (message "trying to add %s" proj)
 	  (setq _cow-proj-handler (cons proj _cow-proj-handler)))
       (message "not adding %s" proj))))
 
-(defvar _cow-proj-handler nil
-"Lookup table of project handlers.
+(defun _cowguts-files-and-dirs-by-prefix (dir prefix)
+  "Return a list of file and directories located in DIR starting with PREFIX"
+  (if (not (file-directory-p dir)) (error "DIR is not a directory"))
+  
+  (directory-files dir t (concat "^" (regexp-quote prefix))))
 
-This lookup table is a list of dotted pairs.  See
-cow-register-project-type for information about the format of the
-dotted pair.")
+(defun cowguts-dirs-by-prefix (dir prefix)
+    "Return a list of all directories in DIR that start with PREFIX
+
+All directories returned will be fully qualified have a trailing
+slash."
+    (mapcar #'file-name-as-directory
+	    (cl-remove-if-not #'file-directory-p
+			      (_cowguts-files-and-dirs-by-prefix dir prefix))))
+(defun cowguts-files-by-prefix (dir prefix)
+    "Return a list of all directories in DIR that start with PREFIX
+
+All directories returned will be fully qualified have a trailing
+slash."
+    (cl-remove-if #'file-directory-p
+		  (_cowguts-files-and-dirs-by-prefix dir prefix)))
+
+(defun cowguts-handle-// (f)
+  "Handle a // in a pathname.
+
+If f does not contain any repeated slashes then f is returned
+unaltered.  Otherwise this returns the portion of the string that
+comes after the last repeated slashes."
+
+  (if (not (cl-search "//" f)) f
+    (let* ((remaining (car (last  (split-string f "//+")))))
+      (cond
+       ((or (string-prefix-p "~/"  remaining)
+	    (string-prefix-p "./"  remaining)
+	    (string-prefix-p "../" remaining)
+	    (equal "."  remaining)
+	    (equal ".." remaining)
+	    (equal "~"  remaining))
+	remaining)
+       (t (concat "/" remaining))))))
+
+	                  
+(defvar _cow-proj-handler nil
+"Table of project handlers.
+
+This lookup table contains a list of dotted pairs.  The first
+element is a predicate function.  The second element is a setup
+function.  See cow-register-project-type for more information.")
 
 (defvar _cow-project-info nil
-  "Association list with information about the project including
-project file name and a function to compile the project. ")
+  "Association list with information about the current project
+including project file name and a function to compile the
+project. ")
   
 
 
