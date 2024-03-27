c26cfe6b91198bc5b2f19b1f3a339cbdf7b1d626
Craig Spannring <craig.spannring@montanainstruments.com>
2024-03-27 15:32:49 -0600

    Add bindings for F9.

--- dot.elisp-redo2023/cow-global-keys.el
+++ dot.elisp-redo2023/cow-global-keys.el
@@ -4,10 +4,15 @@
 (global-set-key "\C-cn" 'find-name-dired)
 (global-set-key "\C-cl" 'find-grep-dired)
 
-(global-set-key [f12] 'compare-windows)
-(global-set-key [f6] 'other-window) 
+(global-set-key [f12]   'compare-windows)
+(global-set-key [f6]    'other-window) 
+(global-set-key [f9 ?c] 'cow-compile-project)
+(global-set-key [f9 f9] 'next-error)
+(global-set-key [f5 ?c] 'compile)
 
-(cond (nil
+
+
+(cond (nil ;; For now, don't use this next set of mappings. 
         ;;
         ;; setup keys to make it easier to step in gdb
         ;; 
@@ -20,7 +25,9 @@
         (define-key gud-mode-map [kp-right] 'gud-next)
         (define-key gud-mode-map [kp-down]  'gud-step)
         (define-key gud-mode-map [kp-up]    'gud-finish)
+	
         (when (equal system-type 'darwin)
+	  ;; Special keyboard setup for a Mac.
           (define-key c++-mode-map [s-right] 'gud-next)
           (define-key c++-mode-map [s-down]  'gud-step)
           (define-key c++-mode-map [s-up]    'gud-finish)
@@ -30,10 +37,5 @@
           (define-key gud-mode-map [s-right] 'gud-next)
           (define-key gud-mode-map [s-down]  'gud-step)
           (define-key gud-mode-map [s-up]    'gud-finish))))
-
-(global-set-key [f9 ?c] 'cow-compile-project)
-(global-set-key [f9 f9] 'next-error)
-(global-set-key [f5 ?c] 'compile)
-
 
 (provide 'cow-global-keys)
