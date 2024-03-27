64d9fcefd8b82a359fff8c7605b6495bef1a1691
Craig Spannring <craig.spannring@montanainstruments.com>
2024-03-27 15:33:58 -0600

    Add customs for show clock and selecting transient marks.

--- dot.elisp-redo2023/cow-post-config.el
+++ dot.elisp-redo2023/cow-post-config.el
@@ -2,14 +2,21 @@
 ;;;
 ;;; This is a good spot for user choices, e.g. should clock be displayed?
 
-(defcustom cow-show-clock t "Show clock in mode"
+(defcustom cow-show-clock t "Show clock on mode line?"
   :type 'boolean
+  :set #'(lambda (symbol value)
+	   (set-default symbol value)
+	   (display-time-mode (if cow-show-clock 1 -1)))
   :group 'cow-emacs-conf)
-(display-time-mode (if cow-show-clock 1 -1))
 
 (show-paren-mode 1)
 
 (defcustom cow-highlight-active-region nil
-  "Should the active region be highlighted") 
-(transient-mark-mode (if cow-highlight-active-region 1 -1))
+  "Should the active region be highlighted"
+  :type 'boolean
+  :set #'(lambda (symbol value)
+	   (set-default symbol value)
+	   (transient-mark-mode (if cow-highlight-active-region 1 -1)))
+  :group 'cow-emacs-conf)
 
+(setenv "PAGER" "cat")
