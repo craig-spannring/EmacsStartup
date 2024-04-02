41d40e4ca673f6516d30a208360ec8b4498deeeb
Craig Spannring <craig.spannring@montanainstruments.com>
2024-04-02 13:56:33 -0600

    Add keybindings to help with gud.

--- dot.elisp-redo2023/cow-gud-setup.el
+++ dot.elisp-redo2023/cow-gud-setup.el
@@ -3,31 +3,33 @@
 ;;;
 
 (require 'cow-cpp-setup)
+(require 'gud)
+(require 'cc-mode)
 
-;; ;;
-;; ;; setup keys to make it easier to step in gdb
-;; ;; 
-;; (define-key c++-mode-map [kp-right] 'gud-next)
-;; (define-key c++-mode-map [kp-down]  'gud-step)
-;; (define-key c++-mode-map [kp-up]    'gud-finish)
-;; (define-key c-mode-map   [kp-right] 'gud-next)
-;; (define-key c-mode-map   [kp-down]  'gud-step)
-;; (define-key c-mode-map   [kp-up]    'gud-finish)
-;; (define-key gud-mode-map [kp-right] 'gud-next)
-;; (define-key gud-mode-map [kp-down]  'gud-step)
-;; (define-key gud-mode-map [kp-up]    'gud-finish)
+;;
+;; setup keys to make it easier to step in gdb
+;; 
+(define-key c++-mode-map [kp-right] 'gud-next)
+(define-key c++-mode-map [kp-down]  'gud-step)
+(define-key c++-mode-map [kp-up]    'gud-finish)
+(define-key c-mode-map   [kp-right] 'gud-next)
+(define-key c-mode-map   [kp-down]  'gud-step)
+(define-key c-mode-map   [kp-up]    'gud-finish)
+(define-key gud-mode-map [kp-right] 'gud-next)
+(define-key gud-mode-map [kp-down]  'gud-step)
+(define-key gud-mode-map [kp-up]    'gud-finish)
 
-;; (when (equal system-type 'darwin)
-;;   ;; Special keyboard setup for a Mac.
-;;   (define-key c++-mode-map [s-right] 'gud-next)
-;;   (define-key c++-mode-map [s-down]  'gud-step)
-;;   (define-key c++-mode-map [s-up]    'gud-finish)
-;;   (define-key c-mode-map   [s-right] 'gud-next)
-;;   (define-key c-mode-map   [s-down]  'gud-step)
-;;   (define-key c-mode-map   [s-up]    'gud-finish)
-;;   (define-key gud-mode-map [s-right] 'gud-next)
-;;   (define-key gud-mode-map [s-down]  'gud-step)
-;;   (define-key gud-mode-map [s-up]    'gud-finish))
+(when (equal system-type 'darwin)
+  ;; Special keyboard setup for a Mac.
+  (define-key c++-mode-map [s-right] 'gud-next)
+  (define-key c++-mode-map [s-down]  'gud-step)
+  (define-key c++-mode-map [s-up]    'gud-finish)
+  (define-key c-mode-map   [s-right] 'gud-next)
+  (define-key c-mode-map   [s-down]  'gud-step)
+  (define-key c-mode-map   [s-up]    'gud-finish)
+  (define-key gud-mode-map [s-right] 'gud-next)
+  (define-key gud-mode-map [s-down]  'gud-step)
+  (define-key gud-mode-map [s-up]    'gud-finish))
 
 
 (provide 'cow-gud-setup)
