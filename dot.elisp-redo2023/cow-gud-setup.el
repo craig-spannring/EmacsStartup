e56157c92c85003f920771b72a6a7583f8a1d1e0
Craig Spannring <craig.spannring@montanainstruments.com>
2024-06-13 10:16:17 -0600

    Rethink how keybindings are organized.

--- dot.elisp-redo2023/cow-gud-setup.el
+++ dot.elisp-redo2023/cow-gud-setup.el
@@ -6,30 +6,5 @@
 (require 'gud)
 (require 'cc-mode)
 
-;;
-;; setup keys to make it easier to step in gdb
-;; 
-(define-key c++-mode-map [kp-right] 'gud-next)
-(define-key c++-mode-map [kp-down]  'gud-step)
-(define-key c++-mode-map [kp-up]    'gud-finish)
-(define-key c-mode-map   [kp-right] 'gud-next)
-(define-key c-mode-map   [kp-down]  'gud-step)
-(define-key c-mode-map   [kp-up]    'gud-finish)
-(define-key gud-mode-map [kp-right] 'gud-next)
-(define-key gud-mode-map [kp-down]  'gud-step)
-(define-key gud-mode-map [kp-up]    'gud-finish)
-
-(when (equal system-type 'darwin)
-  ;; Special keyboard setup for a Mac.
-  (define-key c++-mode-map [s-right] 'gud-next)
-  (define-key c++-mode-map [s-down]  'gud-step)
-  (define-key c++-mode-map [s-up]    'gud-finish)
-  (define-key c-mode-map   [s-right] 'gud-next)
-  (define-key c-mode-map   [s-down]  'gud-step)
-  (define-key c-mode-map   [s-up]    'gud-finish)
-  (define-key gud-mode-map [s-right] 'gud-next)
-  (define-key gud-mode-map [s-down]  'gud-step)
-  (define-key gud-mode-map [s-up]    'gud-finish))
-
-
 (provide 'cow-gud-setup)
+(require 'cow-keys-gud)
