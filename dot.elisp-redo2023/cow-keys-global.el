64e743447a235c2a51f75dfa34d7b826276478b6
Craig Spannring <ns-hub@spannring.org>
2024-10-02 16:19:59 -0600

    Remap home and end kyes.
    Home and end keys should go to the beginning and end of current line,
    not current buffer.

--- dot.elisp-redo2023/cow-keys-global.el
+++ dot.elisp-redo2023/cow-keys-global.el
@@ -6,5 +6,9 @@
 (global-set-key [f9 f9] 'next-error)
 (global-set-key [f5 ?c] 'compile)
 
+(when (equal system-type 'darwin)
+  ;; Special keyboard setup for a Mac.
+  (global-set-key [home] 'move-beginning-of-line)
+  (global-set-key [end] 'move-end-of-line))
 
 (provide 'cow-keys-global)
