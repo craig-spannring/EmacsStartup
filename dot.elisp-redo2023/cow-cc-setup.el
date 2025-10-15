caaae1dacbc9752f69a4cd0c65c50a3830008f3b
Craig Spannring <ns-hub@spannring.org>
2025-10-15 02:15:53 -0600

    Fix problem with indentations.
    Put gnu, k&r, etc styles back into the c-style-alist.

--- dot.elisp-redo2023/cow-cc-setup.el
+++ dot.elisp-redo2023/cow-cc-setup.el
@@ -4,42 +4,65 @@
 
 (message "Loading settings for cc-mode.")
 
-(defcustom cow-cc-coding-standard "MI-STYLE"
-  "Which coding standard are we working with right now?"
-  :type '(string)
-  :options '("PI-STYLE" "MI-STYLE", "stroustrup", "whitesmith", "linux"))
 
-(mapc (lambda (style)  
-        (if (not (boundp 'c-style-alist))
-            (setq c-style-alist style)
-          (setq c-style-alist 
-                (append (list style)
-                        c-style-alist))))
-      '(("PI-STYLE"
-         (c-basic-offset . 3)
-         (c-offsets-alist . (
-                             (substatement-open . 0)
-                             (case-label . +)
-                             (arglist-intro . +))))
-        ("NOT-MI-STYLE"
-         (c-basic-offset . 4)
-         (c-comment-only-line-offset . 0)
-         (c-offsets-alist
-          (statement-block-intro . +)
-          (knr-argdecl-intro . 0)
-          (substatement-open . 0)
-          (substatement-label . 0)
-          (label . 0)
-          (statement-cont . +)))       
-        ("MI-STYLE" ;; Montana Instruments         
-         (c-basic-offset . 4)
-         (c-offsets-alist . ((inclass . ++)
-                             (access-label . -)
-                             (member-init-intro . 0)
-                             (substatement-open . 0)
-                             (case-label . +)
-                             (inlambda . -)
-                             (arglist-intro . +))))))
+(c-add-style "PI-STYLE"
+             '((c-basic-offset . 3)
+               (c-offsets-alist . (
+                                   (substatement-open . 0)
+                                   (case-label . +)
+                                   (arglist-intro . +)))))
+
+(c-add-style  "NOT-MI-STYLE"
+              '((c-basic-offset . 4)
+                (c-comment-only-line-offset . 0)
+                (c-offsets-alist
+                 (statement-block-intro . +)
+                 (knr-argdecl-intro . 0)
+                 (substatement-open . 0)
+                 (substatement-label . 0)
+                 (label . 0)
+                 (statement-cont . +))))
+
+(c-add-style "MI-STYLE" ;; Montana Instruments         
+             '((c-basic-offset . 4)
+               (c-offsets-alist . ((inclass . ++)
+                                   (access-label . -)
+                                   (member-init-intro . 0)
+                                   (substatement-open . 0)
+                                   (case-label . +)
+                                   (inlambda . -)
+                                   (arglist-intro . +)))))
+
+
+(defcustom cow-cc-coding-standard "whitesmith"
+  "Which coding standard are we working with right now?"
+  ;;                         cap     lower
+  :type '(choice (const :tag "MI-STYLE"       "mi-style")
+                 (const :tag "NOT-MI-STYLE"   "not-mi-style")
+                 (const :tag "PI-STYLE"       "pi-style")
+                 (const :tag "Gnu"            "gnu")
+                 (const :tag "K&R"            "k&r")
+                 (const :tag "BSD"            "bsd")
+                 (const :tag "Stroustrup"     "stroustrup")
+                 (const :tag "Whitesmith"     "whitesmith")
+                 (const :tag "Ellemtel"       "ellemtel")
+                 (const :tag "linux"          "linux")
+                 (const :tag "Python"         "python")
+                 (const :tag "Java"           "java")
+                 (const :tag "Awk"            "awk"))          
+  :options '("mi-style"      ; These are offered as convenient choices in the Customize UI
+             "not-mi-style"  
+             "pi-style"      
+             "gnu"           
+             "k&r"           
+             "bsd"           
+             "stroustrup"    
+             "whitesmith"    
+             "ellemtel"      
+             "linux"         
+             "python"        
+             "java"          
+             "awk"))
 
 (add-hook 'c-mode-common-hook
           #'(lambda ()
